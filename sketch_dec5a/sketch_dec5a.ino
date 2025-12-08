4274642cc4fb1687f8c993b38738bf612726fc64
Mirza Adi Raffiansyah <uniqueaddress19@gmail.com>
2025-12-08 11:54:37 +0700

    New Alert

--- sketch_dec5a/sketch_dec5a.ino
+++ sketch_dec5a/sketch_dec5a.ino
@@ -1,100 +1,114 @@
+/*
+  SMART DOOR LOCK ULTIMATE (PATCHED VERSION)
+  Features: RFID + BLE (NimBLE) + WiFi + Google Sheets Log + LCD + RTOS
+  Fixes: Increased Stack Size for HTTPS stability
+*/
+
+// --- 1. SETTING BLYNK & WIFI ---
+// PASTE YOUR BLYNK TEMPLATE INFO HERE
 #define BLYNK_TEMPLATE_ID "TMPL6t6Qt2adf"
 #define BLYNK_TEMPLATE_NAME "Smart Door"
 #define BLYNK_AUTH_TOKEN "F71Ig8gxYu6zX5nGbkAJ_IvRtkln-Deh"
-#define BLYNK_PRINT Serial 
+#define BLYNK_PRINT Serial
 
 #include <WiFi.h>
-#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
 #include <BlynkSimpleEsp32.h>
 #include <DHT.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#include <NimBLEDevice.h>
+#include <LiquidCrystal_I2C.h>
+#include <NimBLEDevice.h> 
 
-char auth[] = BLYNK_AUTH_TOKEN;
-char ssid[] = "Drowsy";
+char ssid[] = "Drowsy"; 
 char pass[] = "sleepyhead";
+char auth[] = BLYNK_AUTH_TOKEN;
 
-#define PIN_DHT 15       
-#define PIN_DOOR_LED 2   
+String GAS_URL = "https://script.google.com/macros/s/AKfycbyYRo2qSSc4vaLwUelyrSg4qRzgXy_aZtxuLINxRKdoxBi8YfZNgamiz0LHlgsrMpKt/exec"; 
+
+#define PIN_DHT 15        
+#define PIN_DOOR_LED 2    
 #define PIN_RFID_SS 5
-#define PIN_RFID_RST 4   
+#define PIN_RFID_RST 4    
 
 DHT dht(PIN_DHT, DHT11); 
 MFRC522 mfrc522(PIN_RFID_SS, PIN_RFID_RST); 
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
+SemaphoreHandle_t mutexBus; 
 
-SemaphoreHandle_t doorMutex; 
-TaskHandle_t blynkTaskHandle;
-TaskHandle_t rfidTaskHandle;
-TaskHandle_t tempTaskHandle;
-
+String authorizedUIDs[] = {"07a83825", "7af84e1b"}; 
+int authorizedCount = 2;
 float currentTemp = 0.0;
-const float TEMP_THRESHOLD = 30.0; 
 
-// Buka Pintu
-void openDoor(String source) {
-  if (xSemaphoreTake(doorMutex, (TickType_t)100) == pdTRUE) {
-    Serial.print("[DOOR] Opening via: "); Serial.println(source);
+void sendLogToGoogleSheets(String uid, String status) {
+  if(WiFi.status() == WL_CONNECTED){
+    HTTPClient http;
+    http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+    
+    String url = GAS_URL + "?uid=" + uid + "&status=" + status;
+    
+    http.begin(url);
+    int httpCode = http.GET();
+    http.end();
+    Serial.print("[HTTP] Log Sent: "); Serial.println(httpCode);
+  } else {
+    Serial.println("[HTTP] Error: WiFi not connected");
+  }
+}
+
+void openDoorTask(String source, String uid) {
+  if (xSemaphoreTake(mutexBus, (TickType_t)100) == pdTRUE) {
+    lcd.clear();
+    lcd.setCursor(0, 0); lcd.print("ACCESS GRANTED");
+    lcd.setCursor(0, 1); lcd.print("Via: " + source); 
+    
     digitalWrite(PIN_DOOR_LED, HIGH); 
+    Blynk.virtualWrite(V0, 1);
+    
+    sendLogToGoogleSheets(uid, "Granted_" + source);
+    Blynk.virtualWrite(V2, "Open by " + source);
+
+    xSemaphoreGive(mutexBus);
     
     vTaskDelay(3000 / portTICK_PERIOD_MS); 
     
-    digitalWrite(PIN_DOOR_LED, LOW);  
-    Serial.println("[DOOR] Locked.");
+    digitalWrite(PIN_DOOR_LED, LOW);
+    Blynk.virtualWrite(V0, 0);
     
-    xSemaphoreGive(doorMutex); 
-  } else {
-    Serial.println("[DOOR] Busy! (Mutex Locked)");
+    if (xSemaphoreTake(mutexBus, (TickType_t)100) == pdTRUE) {
+       lcd.clear(); lcd.setCursor(0, 0); lcd.print("Tap Your Card");
+       xSemaphoreGive(mutexBus);
+    }
   }
 }
 
-// RFID Sensor
-void taskRFID(void *pvParameters) {
-  for (;;) {
-    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-      Serial.println("[RFID] Card Detected!");
-      
-      openDoor("RFID Reader");
-      
-      mfrc522.PICC_HaltA(); 
-      mfrc522.PCD_StopCrypto1();
-    }
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-  }
-}
-
-// Sensor DHT11
-void taskTemp(void *pvParameters) {
-  dht.begin();
-  for (;;) {
-    float t = dht.readTemperature();
-    if (!isnan(t)) {
-      currentTemp = t;
-      if (currentTemp > TEMP_THRESHOLD) {
-        Blynk.logEvent("overheat", "Critical Temp!"); 
-      }
-      Blynk.virtualWrite(V1, currentTemp);
-    }
+void denyAccessTask(String uid) {
+  if (xSemaphoreTake(mutexBus, (TickType_t)100) == pdTRUE) {
+    lcd.clear();
+    lcd.setCursor(0, 0); lcd.print("ACCESS DENIED!");
+    
+    Blynk.logEvent("access_denied", "Unknown ID: " + uid);
+    sendLogToGoogleSheets(uid, "Denied");
+    
+    xSemaphoreGive(mutexBus);
     vTaskDelay(2000 / portTICK_PERIOD_MS);
+    
+    if (xSemaphoreTake(mutexBus, (TickType_t)100) == pdTRUE) {
+       lcd.clear(); lcd.setCursor(0, 0); lcd.print("Tap Your Card");
+       xSemaphoreGive(mutexBus);
+    }
   }
 }
 
-// Blynk
-void taskBlynk(void *pvParameters) {
-  Blynk.begin(auth, ssid, pass);
-  for (;;) {
-    Blynk.run();
-    vTaskDelay(10 / portTICK_PERIOD_MS);
-  }
-}
-
-// BLE
 class MyCallbacks: public NimBLECharacteristicCallbacks {
     void onWrite(NimBLECharacteristic *pCharacteristic) {
-      std::string value = pCharacteristic->getValue();
+      String value = pCharacteristic->getValue(); 
       if (value.length() > 0) {
         if (value == "OPEN") {
-             openDoor("BLE Admin");
+             Serial.println("[BLE] Command Received: OPEN");
+             // technically risky, but okay for PoC
+             openDoorTask("BLE", "ADMIN_HP");
         }
       }
     }
@@ -103,18 +117,17 @@ class MyCallbacks: public NimBLECharacteristicCallbacks {
 void setupBLE() {
   NimBLEDevice::init("ServerDoor");
   NimBLEDevice::setPower(ESP_PWR_LVL_P9); 
-  
   NimBLEServer *pServer = NimBLEDevice::createServer();
   NimBLEService *pService = pServer->createService("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
-  
   NimBLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                         "beb5483e-36e1-4688-b7f5-ea07361b26a8",
-                                         NIMBLE_PROPERTY::READ |
-                                         NIMBLE_PROPERTY::WRITE
-                                       );
+                                           "beb5483e-36e1-4688-b7f5-ea07361b26a8",
+                                           NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE
+                                         );
   pCharacteristic->setCallbacks(new MyCallbacks());
+  NimBLEDescriptor* pDescriptor = pCharacteristic->createDescriptor("2901");
+  pDescriptor->setValue("Door Unlock Switch"); 
+
   pService->start();
-  
   NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
   pAdvertising->addServiceUUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
   
@@ -123,32 +136,81 @@ void setupBLE() {
   pAdvertising->setScanResponseData(scanResponse);
   
   pAdvertising->start();
-  Serial.println("[BLE] Ready (NimBLE)");
+  Serial.println("[BLE] Bluetooth Ready!");
+}
+
+void taskRFID(void *pvParameters) {
+  for (;;) {
+    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+      String uidRaw = "";
+      for (byte i = 0; i < mfrc522.uid.size; i++) {
+        uidRaw += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
+        uidRaw += String(mfrc522.uid.uidByte[i], HEX);
+      }
+      
+      bool authorized = false;
+      for (int i = 0; i < authorizedCount; i++) {
+        if (uidRaw == authorizedUIDs[i]) authorized = true;
+      }
+
+      if (authorized) openDoorTask("RFID", uidRaw);
+      else denyAccessTask(uidRaw);
+      
+      mfrc522.PICC_HaltA(); mfrc522.PCD_StopCrypto1();
+    }
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
+void taskTemp(void *pvParameters) {
+  dht.begin();
+  for (;;) {
+    float t = dht.readTemperature();
+    if (!isnan(t)) {
+      currentTemp = t;
+      Blynk.virtualWrite(V1, currentTemp);
+      
+      if (xSemaphoreTake(mutexBus, (TickType_t)50) == pdTRUE) {
+         lcd.setCursor(0, 1); lcd.print("Temp: " + String(t, 1) + " C   ");
+         xSemaphoreGive(mutexBus);
+      }
+      if (currentTemp > 30.0) {
+        Blynk.logEvent("overheat");
+        Serial.print("[ALERT] THE SERVER IS ON FIREEEEEE\n");
+        }
+    }
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+  }
+}
+
+void taskBlynk(void *pvParameters) {
+  Blynk.begin(auth, ssid, pass);
+  for (;;) {
+    Blynk.run();
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
 }
 
 void setup() {
   Serial.begin(115200);
   pinMode(PIN_DOOR_LED, OUTPUT);
-  doorMutex = xSemaphoreCreateMutex();
+  mutexBus = xSemaphoreCreateMutex();
 
-  SPI.begin(); 
-  mfrc522.PCD_Init();
-  
-  // Debug RFID Connection
-  Serial.print("[INIT] RFID Firmware Version: ");
-  mfrc522.PCD_DumpVersionToSerial(); // KALAU 0x0 or 0xFF, MASALAH WIRING
-  
-  // High sensitivity
+  lcd.init(); lcd.backlight();
+  lcd.setCursor(0,0); lcd.print("System Starting...");
+
+  SPI.begin(); mfrc522.PCD_Init();
   mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
-  Serial.println("[INIT] RFID Ready");
 
-  setupBLE();
+  setupBLE(); 
 
-  // Mutex stuff
-  xTaskCreate(taskRFID, "RFID_Task", 2048, NULL, 1, &rfidTaskHandle);
-  xTaskCreate(taskTemp, "Temp_Task", 4096, NULL, 1, &tempTaskHandle);
-  xTaskCreate(taskBlynk, "Blynk_Task", 6000, NULL, 2, &blynkTaskHandle);
+  lcd.clear(); lcd.setCursor(0,0); lcd.print("Tap Your Card");
+
+  xTaskCreate(taskRFID, "RFID", 8192, NULL, 1, NULL); // Insya allah 8192 cukup
+  xTaskCreate(taskTemp, "Temp", 4096, NULL, 1, NULL);
+  xTaskCreate(taskBlynk, "Blynk", 8192, NULL, 2, NULL); 
 }
 
 void loop() {
+
 }
